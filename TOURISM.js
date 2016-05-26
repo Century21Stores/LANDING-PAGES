6a43633a5aae5ff0357748019690b954832f2f76
Juan Manuel Rios Villanueva <juanmanuelriosv@gmail.com>
2016-05-26 13:40:35 -0400

    TOURSIM Javascript File

---
+++ TOURISM.js
@@ -0,0 +1,16 @@
+function showDetails(element){
+	var parent = element.parentNode.nextElementSibling;
+// 	console.log(parent);
+	if (($(window).width() > 641) && ($(window).width() < 701)) {
+   		parent.classList.toggle("jmrv_mobilePor");
+	}
+	else if ($(window).width() < 640){
+   		parent.classList.toggle("jmrv_mobilePor");
+	}
+	else{
+	console.log(parent);
+	}
+}
+function hideInfo(e){
+	e.classList.remove("jmrv_mobilePor");
+}
